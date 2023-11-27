c6e6199dcafce684c0113b9cd633f2fd52214a37
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-11-27 12:35:06 -0300

    Project finnaly...

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import './App.css';
+import { Fragment, useEffect, useState } from 'react';
 
-import { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
 import AboutMe from './pages/AboutMe';
 import Skill from './pages/Skill';
 import Projects from './pages/Projects';
-import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Container_Footer';
 
 function App() {
+
+  const [scrollY, setScrollY]=useState(0)
+
+  const handleScroll=(c)=>{
+      document.body.scroll(0, c)
+      setScrollY(document.body.scrollTop)
+  }
+  
+  document.body.addEventListener("scroll", ()=>{
+      setScrollY(document.body.scrollTop)
+  })
+
+  useEffect(()=>{
+      const navList = document.querySelector("nav.Navbar")
+
+      document.querySelector(".select").classList.remove("select")
+
+      if (scrollY < 450) {
+
+          navList.childNodes[0].classList.add("select")
+
+      } else if (scrollY < 1050) {
+
+          navList.childNodes[1].classList.add("select")
+
+      } else{
+
+          navList.childNodes[2].classList.add("select")
+      }
+
+  }, [scrollY])
+
   return (
     <Fragment>
-      {/*<HashRouter>
-        <nav>
-          <li><Link to='/'>Sobre min</Link></li>
-          <li><Link to='/skills'>Habilidades</Link></li>
-          <li><Link to='/projects'>Projetos</Link></li>
-        </nav>
-        <Routes>
-          <Route path='/' element={<AboutMy />} />
-          <Route path='/skills' element={<Skill />} />
-          <Route path='/projects' element={<Projects />} />
-        </Routes>
-      </HashRouter>*/}
-      <Navbar />
+      <Navbar handleScroll={handleScroll} />
       <AboutMe />
       <Skill />
       <Projects />
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,40 +1,7 @@
-import { useEffect, useState } from "react"
 import"./Navbar.css"
 import Redes from "./Redes"
 
-function Navbar() {
-
-    const [scrollY, setScrollY]=useState(0)
-
-    const handleScroll=(c)=>{
-        document.body.scroll(0, c)
-        setScrollY(document.body.scrollTop)
-    }
-    
-    document.body.addEventListener("scroll", ()=>{
-        setScrollY(document.body.scrollTop)
-    })
-
-    useEffect(()=>{
-        const navList = document.querySelector("nav.Navbar")
-
-        document.querySelector(".select").classList.remove("select")
-
-        if (scrollY < 450) {
-
-            navList.childNodes[0].classList.add("select")
-
-        } else if (scrollY < 1050) {
-
-            navList.childNodes[1].classList.add("select")
-
-        } else{
-
-            navList.childNodes[2].classList.add("select")
-        }
-
-    }, [scrollY])
-
+function Navbar({ handleScroll }) {
     return (
         <header className="NavBar_Container">
             <Redes />
