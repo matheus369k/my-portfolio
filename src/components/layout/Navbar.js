fdd65ee59edf7870570842e763fca095b3ad67aa
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-11-27 15:25:04 -0300

    updateyng....

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -9,7 +9,7 @@ function Navbar({ handleScroll }) {
             <nav className="Navbar">
                 <li className="select" onClick={(c) => handleScroll(0)}>Sobre min</li>
                 <li onClick={(c) => handleScroll(750)}>Habilidades</li>
-                <li onClick={(c) => handleScroll(1550)}>Projetos</li>
+                <li onClick={(c) => handleScroll(1450)}>Projetos</li>
             </nav>
         </header>
     )
