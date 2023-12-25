e1866e7f91e3bdbf251b83be18e0538a8c90e2d1
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-12-25 18:25:08 -0300

    project updateing style....

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,48 +5,60 @@ import { useEffect, useState } from 'react';
 
 function Navbar() {
 
-    const [scrollY, setScrollY] = useState(0)
-  
-    const handleScroll = (c) => {
-      document.body.scroll(0, c)
-      setScrollY(document.body.scrollTop)
-    }
-  
-    document.body.addEventListener("scroll", () => {
-      setScrollY(document.body.scrollTop)
-    })
-  
-    useEffect(() => {
-      const navList = document.querySelector("nav.Navbar")
-  
-      document.querySelector(".select").classList.remove("select")
-  
-      if (scrollY < 450) {
-  
-        navList.childNodes[0].classList.add("select")
-  
-      } else if (scrollY < 1150) {
-  
-        navList.childNodes[1].classList.add("select")
-  
-      } else {
-  
-        navList.childNodes[2].classList.add("select")
-      }
-  
-    }, [scrollY])
+  const [scrollY, setScrollY] = useState(0)
 
-    return (
-        <header className="NavBar_Container">
-            <Redes />
-            <span className="name">Matheus</span>
-            <nav className="Navbar">
-                <li className="select" onClick={(c) => handleScroll(0)}>Sobre min</li>
-                <li onClick={(c) => handleScroll(750)}>Habilidades</li>
-                <li onClick={(c) => handleScroll(1450)}>Projetos</li>
-            </nav>
-        </header>
-    )
+  const handleScroll = (c) => {
+    document.body.scroll(0, c)
+  }
+
+  document.body.addEventListener("scroll", () => {
+
+    if (document.body.scrollTop === 750) {
+
+      setScrollY(document.body.scrollTop)
+
+    } else if (document.body.scrollTop === 1450) {
+
+
+      setScrollY(document.body.scrollTop)
+
+    }
+  })
+  
+
+  console.log('render navbar')
+
+  useEffect(() => {
+    const navList = document.querySelector("nav.Navbar")
+
+    document.querySelector(".select").classList.remove("select")
+
+    if (scrollY < 450) {
+
+      navList.childNodes[0].classList.add("select")
+
+    } else if (scrollY < 1150) {
+
+      navList.childNodes[1].classList.add("select")
+
+    } else {
+
+      navList.childNodes[2].classList.add("select")
+    }
+
+  }, [])
+
+  return (
+    <header className="NavBar_Container">
+      <Redes />
+      <span className="name">Matheus</span>
+      <nav className="Navbar">
+        <li className="select" onClick={(c) => handleScroll(0)}>Sobre min</li>
+        <li onClick={(c) => handleScroll(750)}>Habilidades</li>
+        <li onClick={(c) => handleScroll(1450)}>Projetos</li>
+      </nav>
+    </header>
+  )
 }
 
 export default Navbar
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -18,9 +18,27 @@ function Projects() {
 
     const url = 'https://matheus369k.github.io/Data/portfolio.json'
 
+    const SkillList = {
+        "HTML5": <FaHtml5 />,
+        "CSS": <FaCss3Alt />,
+        "JavaScript": <IoLogoJavascript />,
+        "React.js": <FaReact />,
+        "JQuery": <SiJquery />,
+        "Sass": <FaSass />
+    }
+
+    const ListStyleIcons = [
+        "html",
+        "css",
+        "javascript",
+        "react",
+        "jquery",
+        "sass"
+    ]
+
     useEffect(() => {
 
-        if (projects.length===0) {
+        if (projects.length === 0) {
             axios(url)
                 .then((resp) => {
 
@@ -30,17 +48,74 @@ function Projects() {
         }
     })
 
-    console.log('render')
+
+    const rendersoftskill = (skils, id) => {
+
+        let listProjects = []
+
+        for (let indexArray = 0; indexArray < Object.values(skils).length; indexArray++) {
+
+            for (let IndexObject = 0; IndexObject < Object.keys(SkillList).length; IndexObject++) {
+
+                if (skils['skill-' + (parseInt(indexArray) + 1)] === Object.keys(SkillList)[IndexObject]) {
+
+                    listProjects.push(
+
+                        <li key={"skill"+id+":"+indexArray} className={`${ListStyleIcons[IndexObject]}`}>
+                            { Object.values(SkillList)[IndexObject] }
+                        </li>
+                    )
+                }
+            }
+        }
+
+        return listProjects
+    }
+
+    console.log('render project')
 
     const renderProjects = () => {
         return (
-            <ul>
+            <ul className={style.project_list}>
                 {
                     projects.map((project) => (
-                        <li key={project.id}>
-                            <h3>{project.name}</h3>
-                            <img src={project.src} alt={`Project: ${project.name}`} />
-                            <p>{project.description}</p>
+                        <li key={project.id} className={style.project}>
+
+                            <h3 className={style.project_name}>{project.name}</h3>
+
+                            <picture className={`${style[project.img]} ${style.project_image}`}></picture>
+
+                            <p className={style.project_description}>{project.description}</p>
+
+                            <div className={style.project_Container_Skill_links}>
+
+                                <nav key={"Links:"+project.id} className={style.project_links}>
+
+                                    <ul>
+
+                                        <li key={"repositorio:"+project.id}  className="repositorio">
+
+                                            <a href={project.links.repositorio}> <CgRemote /> </a>
+
+                                        </li>
+
+                                        <li key={"deploy:"+project.id}  className="site">
+
+                                            <a href={project.links.deploy}> <CgMediaLive /> </a>
+
+                                        </li>
+
+                                    </ul>
+
+                                </nav>
+
+                                <ul key={"ShoftSklls:"+project.id} className={style.project_softSkills}>
+
+                                    {rendersoftskill(project.skils, project.id)}
+
+                                </ul>
+
+                            </div>
                         </li>
                     ))
                 }
