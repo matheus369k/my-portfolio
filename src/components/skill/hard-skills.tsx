3dcde3884867848a6cec8bcb9232116ee3b2a590
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-04-24 19:24:07 -0300

    add: create section skills

--- src/components/skill/hard-skills.tsx
+++ src/components/skill/hard-skills.tsx
@@ -1,17 +1,35 @@
-import react from "../../assets/svgs/react.svg"
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface HardSkill {
+    id: number
+    name: string
+    url: string
+}
 
 export function HardSkills() {
+    const [getHardSkill, setHardSkill] = useState<HardSkill[]>();
+
+    useEffect(() => {
+        axios.get("./src/data/portfolio.json")
+            .then((response) => {
+                setHardSkill(response.data.hardSkill);
+            }, (reject) => {
+                console.log("ERROR:", reject)
+            })
+    }, [])
+
     return (
-        <section id="hardSkills" className="flex flex-col justify-center items-center h-screen max-w-[1149px] mx-auto">
+        <section id="hardSkills" className="flex flex-col mb-20 justify-center items-center h-screen max-w-[1149px] mx-auto">
             <h2 className="text-5xl mb-32">
                 Habilidades Tecnicas
             </h2>
             <ul className="flex flex-wrap items-center gap-20 justify-center w-full">
-                {
-                    Array.from({ length: 10 }).map((_, index)=>(
-                        <li key={index+"Tools"}>
+                {getHardSkill &&
+                    getHardSkill.map(hardSkill=>(
+                        <li key={hardSkill.name+"-"+hardSkill.id}>
                             <img 
-                                src={react} 
+                                src={hardSkill.url} 
                                 alt="tools"
                                 className="size-[100px]"
                             />
