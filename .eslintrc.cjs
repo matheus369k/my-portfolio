ede5cf4f4983b2d776391f5f92793e71585a66e4
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-05-01 15:31:30 -0300

    configured test environment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "react-app/jest",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import { Header } from "./header";
+
+
+describe("Header", () => {
+    test("Render", () => {
+        render(<Header />)
+    })
+})
