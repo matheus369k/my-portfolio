a22494c7398cf895e6e2d4e4ca7dedd16009885d
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-10-19 13:26:00 -0300

    feat: config biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
-  return (
-    <div>
-      <h1 className="font-[family-name:var(--font-chackra-bold)]">hello wolrd</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="font-[family-name:var(--font-chackra-bold)]">
+				Hello World!
+			</h1>
+		</div>
+	);
 }
