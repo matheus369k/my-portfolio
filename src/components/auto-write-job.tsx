ba9eddbff54b2583a2c014e89f49f87ae97229e0
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-11-13 16:51:39 -0300

    feat: create auto write text

---
+++ src/components/auto-write-job.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import TypeIt from 'typeit-react';
+
+export function AutoWriteJob() {
+	return (
+		<p className='text-left w-full font-semibold text-xl flex gap-2'>
+			Eu sou um desenvolvedor
+			<TypeIt
+				className='text-blue-600'
+				options={{
+					strings: ['Web', 'Front-End', 'React'],
+					loop: true,
+					waitUntilVisible: false,
+					lifeLike: true,
+					breakLines: false,
+					startDelete: false,
+					loopDelay: 15000,
+					nextStringDelay: 15000,
+					afterComplete: () => {
+						return new Promise((resolve) => {
+							setTimeout(() => {
+								resolve(true);
+							}, 15000);
+						});
+					},
+					deleteSpeed: 25,
+					startDelay: 0,
+					speed: 50,
+				}}
+			/>
+		</p>
+	);
+}
