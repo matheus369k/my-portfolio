59e23205d22b5ca2cd760ec8fcd86ee2f5e3d9df
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-11-17 16:27:04 -0300

    style: color text and add message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,11 +4,18 @@ import Link from 'next/link';
 export default function NotFound() {
 	return (
 		<div className='flex flex-col items-center gap-2 h-full justify-center'>
-			<div className='flex items-center gap-2 text-red-600'>
-                <ShieldAlert className='size-6 animate-pulse' />
-                <span className='font-semibold'>A pagina não foi encontrada !</span>
-            </div>
-			<Link className='underline font-semibold text-zinc-400' href='/'>Voltar</Link>
+			<div className='flex gap-2'>
+				<ShieldAlert className='size-6 animate-pulse' />
+				<span className='font-semibold max-w-96 text-center'>A pagina não foi encontrada. Clique no botão abaixo para voltar a pagina principal</span>
+			</div>
+			<Link href='/'>
+				<button
+					title='voltar'
+					type='button'
+					className='inline font-bold rounded-lg px-8 py-2 bg-blue-600'>
+					Voltar
+				</button>
+			</Link>
 		</div>
 	);
 }
