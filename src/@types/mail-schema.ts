11f6231ea4360fdbe7ddeb9d9b6ccc44fcaf766e
matheus369k <gomes.craft10@gmail.com>
2026-02-28 18:50:55 -0300

    feat: add  max size validation to email and name in zod schema

--- src/@types/mail-schema.ts
+++ src/@types/mail-schema.ts
@@ -6,13 +6,15 @@ export const FormSchema = z.object({
     .min(1, {
       message: 'Nome é obrigatório',
     })
-    .regex(/^[a-zA-Z].*$/, 'Nome inválido'),
+    .regex(/^[a-zA-Z].*$/, 'Nome inválido')
+    .max(50, 'Nome excede o tamanho máximo'),
   email: z
     .string()
     .min(1, {
       message: 'E-Mail é obrigatório',
     })
-    .email('E-Mail inválido'),
+    .email('E-Mail inválido')
+    .max(50, 'E-Mail excede o tamanho máximo'),
   message: z
     .string()
     .min(15, {
