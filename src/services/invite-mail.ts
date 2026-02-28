9c00dc1e18d766d9f46b13273e27b58a5d40dc85
matheus369k <gomes.craft10@gmail.com>
2026-02-28 19:01:47 -0300

    refactor: direct return of response api

--- src/services/invite-mail.ts
+++ src/services/invite-mail.ts
@@ -1,25 +1,27 @@
-'use server';
+'use server'
 
-import type { FormData, ReturnInviteMail } from "@/@types";
-import { fetchAPI } from "@/lib/axios";
+import type { FormData, ReturnInviteMail } from '@/@types'
+import { fetchAPI } from '@/lib/axios'
 
-export async function inviteMail({ from_name, email, message }: FormData): Promise<ReturnInviteMail> {
-    const { status } = await fetchAPI.post('/invite-email', {
-        from_name,
-        email,
-        message,
-    }
-    ).then((): ReturnInviteMail => {
-        return {
-            status: 'ok'
-        }
-    }).catch((): ReturnInviteMail => {
-        return {
-            status: 'error'
-        }
-    });
-
-    return {
-        status
-    }
+export async function inviteMail({
+  from_name,
+  email,
+  message,
+}: FormData): Promise<ReturnInviteMail> {
+  return await fetchAPI
+    .post('/invite-email', {
+      from_name,
+      email,
+      message,
+    })
+    .then((): ReturnInviteMail => {
+      return {
+        status: 'ok',
+      }
+    })
+    .catch((): ReturnInviteMail => {
+      return {
+        status: 'error',
+      }
+    })
 }
